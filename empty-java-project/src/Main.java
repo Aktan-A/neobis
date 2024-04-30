55fdc2ff8da19ecedffba40f615e8f9bd2303d2b
Aktan A <aktan.amanaliev@gmail.com>
2024-04-30 19:56:52 +0600

    Add a sample java project

---
+++ empty-java-project/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Sorry, this program is just an example of some Java code.");
+        System.out.println("Meaning, it does not do anything interesting...");
+
+        if (name.length() > 10) {
+            System.out.println("But it has identified that you have a pretty long name!");
+        } else {
+            System.out.println("But it has identified that you have a pretty short name!");
+        }
+
+    }
+}
